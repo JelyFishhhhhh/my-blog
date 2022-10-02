0fa941099b3cadc773b60899e29c68617ab1d50b
JelyFishhhhhh_ _ <dlanfongtwb@gmail.com>
2022-10-02 15:26:59 +0800

    Auto-Commit

--- auto_commit.bat
+++ auto_commit.bat
@@ -1,6 +1,6 @@
 set dir=%cd%
 cd %dir%
 git add .
-git commit -m "---%date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%---Auto-Commit"
+git commit -m "Auto-Commit"
 git push -u origin main
 exit
