44abd10d76bcd34504b32743c449c3fe0c3edbee
JelyFishhhhhh_ _ <dlanfongtwb@gmail.com>
2022-10-02 14:45:59 +0800

    init commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/', methods=['GET', 'POST'])
+def origin():
+    return render_template('root.html')
+
+@app.route('/templates/<filename>', methods=['GET', 'POST'])
+def template(filename):
+    return render_template(filename)
+
+if __name__ == '__main__':
+    
+    with open('.\\info\\config.json', 'r', encoding='utf-8') as file:
+        config = json.load(file)
+    
+    app.run(
+        
+        host = config['app_info']['host'], 
+        port = config['app_info']['port'],
+        debug = config['app_info']['debug'],  
+        use_reloader = config['app_info']['use_reloader'] 
+    )
---
+++ static/js/import.js
@@ -0,0 +1,17 @@
+function includeHTML(i=0) {
+    let include_list = document.getElementsByTagName("ih");
+    if (i >= include_list.length) {
+        return
+    }
+    let element = include_list[i];
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            if (this.status == 404) {element.innerHTML = "Page not found.";}
+            else {element.innerHTML = this.responseText;}
+        }
+    }
+    xhttp.open("GET", `/templates/${element.id}.html`, true);
+    xhttp.send();
+    setTimeout(includeHTML, 100, i+1)
+}
